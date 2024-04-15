60642a8042b2ed45041a562513d0c70432d8fecf
Mohammad Javad <126692128+hejazij88@users.noreply.github.com>
2024-04-15 11:34:29 +0330

    Add files via upload

---
+++ ROP/ActionResult.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROP
+{
+    public class ActionResult
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+
+
+        public static ActionResult<T> CreateValidator<T>(T param) =>
+            new(param);
+
+        public static ActionResult<T> Failure<T>(string message)
+            => new(message);
+
+        public static ActionResult<T> Success<T>(T data)
+            => new(data);
+    }
+    public class ActionResult<T>:ActionResult
+    {
+        public T Data { get; set; }
+
+        public ActionResult(string message)
+        {
+            Message=message;
+            IsSuccess = false;
+        }
+
+        public ActionResult(T data)
+        {
+            IsSuccess = true;
+            Data=data;
+        }
+    }
+
+    public static class ResultExtension
+    {
+        public static ActionResult<T> Validate<T>(this ActionResult<T> actionResult,
+            Func<T, bool> predict, string message)
+        {
+            if (!actionResult.IsSuccess)
+            {
+                return actionResult;
+            }
+
+            var predicateResult = predict(actionResult.Data);
+            if (predicateResult)
+            {
+                return ActionResult.Failure<T>(message);
+            }
+
+            return actionResult;
+        }
+    }
+}
---
+++ ROP/UserServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROP
+{
+    class UserServices
+    {
+        public ActionResult<int> UpdateProfile(User user)
+        {
+            //if (string.IsNullOrEmpty(user.Name))
+            //    return ActionResult.Failure<int>("Name Is Required");
+
+            //if (user.Name.Length > 50)
+            //    return ActionResult.Failure<int>("Name Must Be Less Than 50");
+
+            //if(string.IsNullOrEmpty(user.Email))
+            //    return ActionResult.Failure<int>("Email Is Required");
+
+
+            var result = ActionResult.CreateValidator(user)
+                .Validate(x => string.IsNullOrEmpty(user.Name), "Name Is Required")
+                .Validate(x => user.Name.Length > 50, "Name Is Required")
+                .Validate(x => string.IsNullOrEmpty(user.Email), "Email Is Required");
+
+            if (!result.IsSuccess)
+                return ActionResult.Failure<int>(result.Message);
+
+                return ActionResult.Success<int>(15151);
+        }
+    }
+}
